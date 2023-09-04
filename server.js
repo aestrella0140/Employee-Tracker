8ebe7de1f7ded18e043f8e60016ca976f1110911
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-09-04 16:58:15 -0700

    promptuser finished promise if else statements

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 
+const fs = require('fs').promises;
 
-const express = require('express');
+const express = require("express");
 // import require mysql2
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,25 +15,53 @@ app.use(express.json());
 
 // connect to database
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // username 
-        user: '',
-        // find a way to not put personal mysql password
-        password: '',
-        database: ''
-    },
-    // create a database then add name before database
-    console.log('connected to database')
+  {
+    host: "localhost",
+    // username
+    user: "",
+    // find a way to not put personal mysql password
+    password: "",
+    database: "",
+  },
+  // create a database then add name before database
+  console.log("connected to database")
 );
 
 // inquirer prompt
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: "table",
-            name: "",
-            
-        }
-    ])
-}
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "choices",
+      message: "Please select an option",
+      choices: [
+        "view all departments",
+        "view all roles",
+        "view all employees",
+        "add a department",
+        "add a role",
+        "add an employee",
+        "update an employee role",
+      ]
+    }
+  ])
+// promise return
+  .then((answers) => {
+    const { choices } = answers;
+    if(answers === "view all departments") {
+        viewAllDepartments();
+    }else if(answers === "view all roles") {
+        viewAllRoles();
+    }else if(answers === "view all employees") {
+        viewAllEmployees();
+    }else if(answers === "add a department") {
+        addADepartment();
+    }else if(answers === "add a role") {
+        addARole();
+    }else if(answers === "add an employee") {
+        addAnEmployee();
+    }else if(answers === "update an employee role") {
+        updateAnEmployeeRole();
+    }
+  })
+};
